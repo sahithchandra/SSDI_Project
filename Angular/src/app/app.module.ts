5e4f55aa2abc3a2efab199d92ebfab618c6bfb24
gong-boy <59751825+gong-boy@users.noreply.github.com>
2020-02-21 08:47:27 -0500

    Add files via upload

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {DataTablesModule} from 'angular-datatables';
+import {StudentListComponent} from './features/components/student-list/student-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DataTablesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
